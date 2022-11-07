bc76cae1226a16887ef9a0383fda6235df3afe2c
AndjelaDavidovic <andjelamdavidovic07@gmail.com>
2022-11-07 08:08:06 +0100

    Prvi komit

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- Container wrapper -->
+  <div class="container">
+    <!-- Navbar brand -->
+    <a class="navbar-brand me-2" href="https://mdbgo.com/">
+      <img
+        src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+        height="16"
+        alt="MDB Logo"
+        loading="lazy"
+        style="margin-top: -1px;"
+      />
+    </a>
+
+    <!-- Toggle button -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarButtonsExample"
+      aria-controls="navbarButtonsExample"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarButtonsExample">
+      <!-- Left links -->
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Dashboard</a>
+        </li>
+      </ul>
+      <!-- Left links -->
+
+      <div class="d-flex align-items-center">
+      <a href="logovanje.php" class="btn"><i class="fab fa-github"></i> Prijavi se</a>
+      <a href="registracija.php" class="btn"><i class="fab fa-github"></i> Registracija</a>
+        <a
+          class="btn btn-dark px-3"
+          href="https://github.com/mdbootstrap/mdb-ui-kit"
+          role="button"
+          ><i class="fab fa-github"></i
+        ></a>
+      </div>
+    </div>
+    <!-- Collapsible wrapper -->
+  </div>
+  <!-- Container wrapper -->
+</nav>
+<!-- Navbar -->
+<br>
+<a href="tema.php" class="btn"><i class="fab fa-github"></i> Kreiraj temu</a>
+<div class="container mx-auto mt-4">
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+       <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
+    <a href="#" class="btn "><i class="fab fa-github"></i> Github</a>
+  </div>
+  </div>
+    </div>    
+       <div class="col-md-4">
+<div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn  mr-2"><i class="fas fa-link"></i> Visit Site</a>
+    <a href="#" class="btn"><i class="fab fa-github"></i> Github</a>
+  </div>
+  </div>
+    </div>    
+          <div class="col-md-4">
+<div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
+    <a href="#" class="btn "><i class="fab fa-github"></i> Github</a>
+  </div>
+  </div>
+  </div>
+    
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+       <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
+    <a href="#" class="btn"><i class="fab fa-github"></i> Github</a>
+  </div>
+  </div>
+    </div>    
+       <div class="col-md-4">
+<div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
+    <a href="#" class="btn"><i class="fab fa-github"></i> Github</a>
+  </div>
+  </div>
+    </div>    
+          <div class="col-md-4">
+<div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
+    <a href="#" class="btn"><i class="fab fa-github"></i> Github</a>
+  </div>
+  </div>
+  </div>
+    
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+       <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
+    <a href="#" class="btn"><i class="fab fa-github"></i> Github</a>
+  </div>
+  </div>
+    </div>    
+       <div class="col-md-4">
+<div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
+    <a href="#" class="btn"><i class="fab fa-github"></i> Github</a>
+  </div>
+  </div>
+    </div>    
+          <div class="col-md-4">
+<div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary mr-2"><i class="fas fa-link"></i> Visit Site</a>
+    <a href="#" class="btn btn-primary"><i class="fab fa-github"></i> Github</a>
+  </div>
+  </div>
+  </div>
+    
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+       <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
+    <a href="#" class="btn"><i class="fab fa-github"></i> Github</a>
+  </div>
+  </div>
+    </div>    
+       <div class="col-md-4">
+<div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
+    <a href="#" class="btn"><i class="fab fa-github"></i> Github</a>
+  </div>
+  </div>
+    </div>    
+          <div class="col-md-4">
+<div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
+    <a href="#" class="btn"><i class="fab fa-github"></i> Github</a>
+  </div>
+  </div>
+  </div>
+</div>
+  </div>
+</body>
+</html>
